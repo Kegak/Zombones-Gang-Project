63a64ae5fefe74c077fe76e1b8f428a4b67ec112
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2023-10-18 22:53:15 -0500

    Updated AI to stop following after distance

--- Assets/MainAssets/Scripts/AIController.cs
+++ Assets/MainAssets/Scripts/AIController.cs
@@ -8,19 +8,35 @@ public class AIController : MonoBehaviour
 
     private GameObject destination;
     private NavMeshAgent agent;
+    private Vector3 targetPos;
+    private float targetDist;
 
     // Start is called before the first frame update
     void Start()
     {
         destination = GameObject.FindGameObjectWithTag("Player");
-
+        
         agent = GetComponent<NavMeshAgent>();
-
-        // agent.SetDestination(destination.transform.position);
     }
 
     void Update() 
     {
-        agent.SetDestination(destination.transform.position);
+        targetPos = destination.transform.position;
+        targetDist = Vector3.Distance(targetPos, transform.position);
+
+        Debug.Log(targetDist);
+
+        if(targetDist < 12)
+        {
+            agent.SetDestination(destination.transform.position);
+        }
+
+        else
+        {
+            agent.isStopped = true;
+            agent.ResetPath();
+
+            // TODO - Set idle animation here as well
+        }
     }
 }
