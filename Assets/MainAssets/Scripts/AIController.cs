abdc9081d4d9f19c36b6e4b2a598369492a2ec07
Kegak <keeganjsims@gmail.com>
2023-10-19 02:51:33 -0500

    Added Zombie with Animations and Sounds

--- Assets/MainAssets/Scripts/AIController.cs
+++ Assets/MainAssets/Scripts/AIController.cs
@@ -10,6 +10,8 @@ public class AIController : MonoBehaviour
     private NavMeshAgent agent;
     private Vector3 targetPos;
     private float targetDist;
+    AudioSource groan;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,10 @@ public class AIController : MonoBehaviour
         destination = GameObject.FindGameObjectWithTag("Player");
         
         agent = GetComponent<NavMeshAgent>();
+
+        groan = GetComponent<AudioSource>();
+
+        animator = GetComponent<Animator>();
     }
 
     void Update() 
@@ -29,14 +35,46 @@ public class AIController : MonoBehaviour
         if(targetDist < 15)
         {
             agent.SetDestination(destination.transform.position);
+            PlaySound();
+            animator.SetFloat("forward", 1.0f);
         }
 
         else
         {
             agent.isStopped = true;
             agent.ResetPath();
+            groan.Stop();
+            animator.SetFloat("forward", 0.0f);
 
             // TODO - Set idle animation here as well
         }
+        if (targetDist <= agent.stoppingDistance)
+        {
+            animator.SetFloat("forward", 1.0f);
+            FaceTarget();
+        }
+
     }
+
+    void PlaySound()
+    {
+        if (!groan.isPlaying)
+        {
+            groan.Play();
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (targetPos - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,targetDist);
+    }
+
 }
